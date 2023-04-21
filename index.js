26925fcc7386de806329ece841cab9a27ff017e4
ij2356 <ij2356@m-sync.co.kr>
2023-04-21 18:02:52 +0900

    작업중

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ $(function () {
   });
   //section1
   gsap.registerPlugin(ScrollTrigger);
+
   const tl = gsap.timeline();
   tl.from(".section1 .home__first-project-wrapper", { xPercent: 0, yPercent: 0 }).to(".section1", { xPercent: -20, yPercent: -40, opacity: 0, scale: 0.4, duration: 7 });
   ScrollTrigger.create({
     animation: tl,
     trigger: ".section1",
     start: "top top",
-    end: "bottom -30%",
+    end: "bottom -50%",
     pin: true,
     scrub: true,
     anticipatePin: 0,
@@ -21,17 +22,39 @@ $(function () {
 
   //section2
   const tl2 = gsap.timeline();
-  tl2.from(".section2 .home__first-project-wrapper", { xPercent: 100, yPercent: -100, opacity : 0 })
-    .to(".section2 .home__first-project-wrapper", { xPercent: 0, yPercent: 0, opacity: 1 })
-    .to(".section2 .home__first-project-wrapper", { xPercent: -100, yPercent: 100, opacity :0 })
+  tl2
+    .from(".section2 .home__project-media", { xPercent: 20, yPercent: -20, opacity: 0, duration: 4, filter: "blur(12px)" })
+    .to(".section2 .home__project-media", { xPercent: 0, yPercent: 0, opacity: 1, filter: "blur(0px)", duration: 9 })
+    .to(".section2 .home__project-media", { xPercent: -20, yPercent: 20, filter: "blur(12px)", opacity: 0, duration: 4 });
   ScrollTrigger.create({
     animation: tl2,
     trigger: ".section2",
-    start: "top top",
-    end: "bottom -30%",
+    start: "top 80",
+    end: "bottom 30%",
     pin: true,
+    pinSpacing: "margin",
     scrub: true,
-    anticipatePin: 0,
+    anticipatePin: 1,
     markers: true, // 디버깅용 마커 표시
-  })
+  });
+
+  //section2
+  // const tl6 = gsap.timeline();
+  // tl6.fromTo(".section6 .background li:nth-child(1)", { xPercent: "-100%" }, { xPercent: "0%" }); // in from left
+  // tl6.fromTo(".section6 .background li:nth-child(2)", { xPercent: "-100%" }, { xPercent: "0%" }); // in from left
+  // tl6.fromTo(".section6 .background li:nth-child(3)", { xPercent: "-100%" }, { xPercent: "0%" }); // in from left
+  // tl6.fromTo(".section6 .background li:nth-child(4)", { xPercent: "-100%" }, { xPercent: "0%" }); // in from left
+    
+    
+  // ScrollTrigger.create({
+  //   animation: tl6,
+  //   trigger: ".section6",
+  //   start: "top top",
+  //   end: "bottom 30%",
+  //   pin: true,
+  //   pinSpacing: "margin",
+  //   scrub: true,
+  //   anticipatePin: 0,
+  //   markers: true, // 디버깅용 마커 표시
+  // });
 });
